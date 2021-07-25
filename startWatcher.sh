c1cc502505b8c7fc8bf6d9e6a883a91db392c26c
Jeff Goeke-Smith <jeff@goekesmi.com>
2021-07-25 01:57:20 +0000

    Set the base point as a variable

--- startWatcher.sh
+++ startWatcher.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+BUCKETBASE=s3://dc-spekops/DVR/
+
 if [ $# -eq 0 ]; then
 	echo "No arguments supplied. Expecting run data directory."
 	exit 1;
@@ -19,6 +21,6 @@ while true; do
 	URL=`cat URL`
 	youtube-dl --external-downloader-args '-loglevel error -stats'  $URL
 	CHANNEL=${PWD##*/}
-	find . -type f -iname '*.mp4' -print0 | xargs -0 -P3 -I{} aws s3 mv {} s3://dc-spekops/DVR/$CHANNEL/ 
+	find . -type f -iname '*.mp4' -print0 | xargs -0 -P3 -I{} aws s3 mv {} $BUCKETBASE/$CHANNEL/ 
 	sleep 40;
 done;
